f9fa514cb664d8a4b773d9b127aa1d9bf42ee392
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2026-01-26 13:59:04 -0700

    adds showcase work section

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,28 @@
+
+const ShowcaseSection = () => {
+  return (
+    <div id="work" className="app-showcase">
+        <div className="w-full">
+            <div className="showcaselayout">
+                {/* LEFT */}
+                <div className="first-project-wrapper">
+                    <div className="image-wrapper">
+                        <img src="/images/project1.png" alt="Ryde"/>
+                    </div>
+                    <div className="text-content">
+                        <h2>
+                            On-Demand Rides Made Simple with a Powerful, User-Friendly App called Ryde
+                        </h2>
+                        <p className="text-white-50 md:text-xl">
+                            An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                        </p>
+                    </div>
+                </div>
+                {/* RIGHT */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShowcaseSection
