e043467a8fa7c7136a6b6ac6b441a973b5bea342
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2026-01-26 13:29:27 -0700

    adds animated counter with career milestones

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,22 @@
 
 const Button = ({ className, id, text }) => {
   return (
-    <a className={`${className ?? ''} cta-wrapper`}>
+    <a
+      className={`${className ?? ''} cta-wrapper`}
+      onClick={(e) => {
+        e.preventDefault();
+
+        const target = document.getElementById('counter');
+
+        if (target && id) {
+          const offset = window.innerHeight * 0.15;
+
+          const top = target.getBoundingClientRect().top + window.scrollY - offset;
+
+          window.scrollTo({ top, behavior: 'smooth' })
+        }
+      }}
+    >
         <div className="cta-button group">
             <div className="bg-circle" />
             <p className="text">{text}</p>
--- src/components/HeroModels/Room.jsx
+++ src/components/HeroModels/Room.jsx
@@ -42,7 +42,7 @@ export function Room(props) {
       <EffectComposer>
         <SelectiveBloom
           selection={screensRef}
-          intensity={1.5} // Strength of the bloom
+          intensity={0.4} // Strength of the bloom
           luminanceThreshold={0.2} // Minimum luminance needed
           luminanceSmoothing={0.9} // Smooth transition
           blendFunction={BlendFunction.ADD} // How it blends
