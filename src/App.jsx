bfeeb3cbde7ad731fca80ceaec1aaf0814ab0650
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2026-01-27 12:56:00 -0700

    adds logo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero"
 import AppShowcase from "./sections/AppShowcase"
 import NavBar from "./components/NavBar"
+import LogoSection from "./components/LogoSection"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <NavBar/>
     <Hero/>
     <AppShowcase/>
+    <LogoSection/>
    </div>
   )
 }
